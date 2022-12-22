1284eee169038dfe0498ba596352420e28d5818c
Mayank-3e <111231656+Mayank-3e@users.noreply.github.com>
2022-12-22 21:24:14 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import About from './components/About';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Alert from './components/Alert';
+import Loading from "./components/Loading";
 
 function App() {
   return (
   <>
     <Navbar/>
     <Alert/>
+    <Loading/>
     <div className='container'>
       <Routes>
         <Route path='/' element={<Home/>}/>
